6c01b8e3e85ebe9fd70ed2facf016ce458904b23
Laszlo Frazer <z3514778@katana.science.unsw.edu.au>
2017-05-29 11:49:59 +1000

    added the ability to select a single input row so each CPU on the supercomputer only gets one process running

---
+++ rbatchsc.sh
@@ -0,0 +1,28 @@
+#runs R script to generate a bunch of configurations, then runs simulation
+
+starttime=$(date +%d%m%H%M%S);
+echo "Current (mA/cm2)	bins	depth (cm)	k1	k2	eta_c	sensitizer concentration (M)	solar concentration factor	minwavelength	maxwavelength" > $starttime.out.dat
+
+#unnecessary loop goes over the row selected by PBS_ARRAYID
+while read p; do
+  exec ./a.out $p >>$starttime.out.dat &
+done < <(Rscript generator.R | sed -n "$PBS_ARRAYID"'p')
+
+echo "Started Calculation"
+
+FAIL=0
+
+
+for job in `jobs -p`
+do
+    wait $job || let "FAIL+=1"
+done
+
+echo $FAIL
+
+if [ "$FAIL" == "0" ];
+then
+echo "Success"
+else
+echo "FAIL! ($FAIL)"
+fi
